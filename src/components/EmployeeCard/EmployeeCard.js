d58374b022f99247dcfd817513bd4474447d82d3
JD <janelle.m.deane@gmail.com>
2020-11-05 16:21:54 -0800

    added employee card and search

--- src/components/EmployeeCard/EmployeeCard.js
+++ src/components/EmployeeCard/EmployeeCard.js
@@ -1,1 +1,12 @@
 import './EmployeeCard.css';
+import React, { Component } from 'react'
+
+export default class EmployeeCard extends Component {
+    render() {
+        return (
+            <div>
+                <h2>EmployeeCard</h2>
+            </div>
+        )
+    }
+}
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,1 +1,13 @@
 import './Search.css';
+
+import React, { Component } from 'react'
+
+export default class Search extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Search Component</h2>
+            </div>
+        )
+    }
+}
