ff82ca7d45b7fde13879275be7b9f00be66a1fd5
JD <janelle.m.deane@gmail.com>
2020-11-05 16:17:56 -0800

    added navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,1 +1,24 @@
 import './NavBar.css';
+import { Link, useLocation} from "react-router-dom";
+
+export default function NavBar() {
+    const location =useLocation();
+    return (
+        <ul className="nav nav-bar">
+            <h2>Employee Directory</h2>
+      <li className="nav-item">
+        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+         Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Discover"
+          className={location.pathname === "/search" ? "nav-link active" : "nav-link"}
+        >
+          Search Directory
+        </Link>
+      </li>
+    </ul>
+    )
+}
